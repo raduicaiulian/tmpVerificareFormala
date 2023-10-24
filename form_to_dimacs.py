112c4a52de565a6402bf9e10bb9cef2277945a06
Raduica Iulian <raduicaiulian@gmail.com>
2023-10-24 04:15:33 +0300

    made parsing loops to accept both AND and OR operators and produce what seems to be the right output

--- form_to_dimacs.py
+++ form_to_dimacs.py
@@ -2,7 +2,7 @@ def expand(formula):
     loop_stack = []
     output_formula = ""
     while(formula):
-        if "and(" in formula or "or(" in formula:
+        if "and(" in formula or "or(" in formula: # parse input formula to a stack of loops
             if "and(" in formula:
                 and_index = formula.index("and(") + 4
             else:
@@ -27,26 +27,32 @@ def expand(formula):
             formula = ""
             dinamic_for_loop="out = ''\nop = ''\n"
             number_of_tabs = 1
-            formula += "out += f\" S("
+            formula = "out += f\"{op} S("
             for loop in loop_stack:
-                print("loop: " + loop)
                 loop_element = loop.split(",")
                 dinamic_for_loop += f"for {loop_element[0]} in range({loop_element[1]}, {str(int(loop_element[2])+1)}):"
                 dinamic_for_loop += "\n" + "\t" * number_of_tabs
-                dinamic_for_loop += f"op = '{loop_element[3]}'"
-                dinamic_for_loop += "\n" + "\t" * number_of_tabs
+                if loop != loop_stack[-1]:
+                    dinamic_for_loop += f"op = '{loop_element[3]}'"
+                    dinamic_for_loop += "\n" + "\t" * number_of_tabs
                 number_of_tabs += 1
-                formula += "{" + loop[0]+ "},"
+                formula += "{" + loop_element[0]+ "},"
                 if loop == loop_stack[-1]:
                     # breakpoint()
-                    dinamic_for_loop += formula[:-1] + ")" + ( " and" if loop_element[3] == "and" else " or") + "\""
-            print(loop_element[3])
-            #dinamic_for_loop += formula[:-1] + ")" + ( " and" if loop_element[3] == "and" else " or") + "\""
-            print(dinamic_for_loop)
-            breakpoint()
-            #print(dinamic_for_loop)
-            exec(dinamic_for_loop) # exposes the extended formula in out var
+                    dinamic_for_loop += formula[:-1] + ") " + "\""
+                    dinamic_for_loop += "\n" + "\t" * (number_of_tabs - 1)
+                    dinamic_for_loop += f"op = '{loop_element[3]}'\n"
+            dinamic_for_loop += 'out = out[out.index(" ")+1:]'
 
+            # for debugging
+            dinamic_for_loop += '\nprint("----------out------------")\n'
+            dinamic_for_loop += 'print(out)\n'
+            dinamic_for_loop += 'print("----------out------------")\n'
+            dinamic_for_loop += 'print("and" in out)\n'
+
+            print(dinamic_for_loop)
+            exec(dinamic_for_loop) # exposes the extended formula in out var
+            breakpoint()
             pass
         print(loop_stack)
 
